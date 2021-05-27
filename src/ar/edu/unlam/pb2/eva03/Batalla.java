dd50f51689d8eb7eac773da635a5c795c2df5491
soldouza <soldouza@hotmail.com>
2021-05-27 17:15:49 -0300

    comienzo de proyecto

---
+++ src/ar/edu/unlam/pb2/eva03/Batalla.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.pb2.eva03;
+
+import java.util.List;
+
+import ar.edu.unlam.pb2.eva03.enumeradores.TipoDeBatalla;
+
+public class Batalla {
+	private Double latitud;
+	private Double longitud;
+	private TipoDeBatalla tipo;
+	private String nombre;
+	private List<Vehiculo> vehiculosEnLaBatalla;
+	
+}
---
+++ src/ar/edu/unlam/pb2/eva03/FuerzaArmada.java
@@ -0,0 +1,11 @@
+package ar.edu.unlam.pb2.eva03;
+
+import java.util.Map;
+import java.util.Set;
+
+public class FuerzaArmada {
+
+	private List<Vehiculo> convoy;
+	private List<Batalla> batallas; 
+
+}
